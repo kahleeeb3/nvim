83f9230823a1dd81bf63dc175e0e022bc1ef01a6
kahleeeb3 <powellcalebm@gmail.com>
2026-04-09 19:30:35 -0500

    space + \ to open terminal

--- lua/kahleeeb3/plugins/toggleterm.lua
+++ lua/kahleeeb3/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
     "akinsho/toggleterm.nvim",
     config = function()
         require("toggleterm").setup({
-            open_mapping = [[<leader>t]],
+            open_mapping = [[<leader>\]],
             direction = "float",
         })
     end,
